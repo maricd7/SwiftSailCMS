0fc090670f64a1fedf9d55bcd3f03eefc0de9ff8
unknown <dejanmaric259@gmail.com>
2024-01-14 21:31:15 +0100

    products overview update

--- app/contexts/ProductsContext.tsx
+++ app/contexts/ProductsContext.tsx
@@ -1,16 +1,33 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import supabase from '../supabase';
 
 const ProductsContext = createContext({
     products:[],
 });
 
-
 export const ProductContextProvider = ({ children }) => { 
 
     const [products, setProducts] = useState([]);
 
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const { data, error } = await supabase
+                    .from('products')
+                    .select('*');
+                
+                if (error) {
+                    throw error;
+                }
 
+                setProducts(data);
+            } catch (error: any) {
+                console.error('Error fetching data:', error.message);
+            }
+        }
 
+        fetchData();
+    }, []);
 
     const contextValue = {
         products: products,
---
+++ app/supabase.tsx
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY, 'asdsaed')
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+
+export default supabase;
