5c734034645498aec1b7601b6ecbd5d4cf54da45
unknown <dejanmaric259@gmail.com>
2024-01-15 18:27:09 +0100

    fixing typescript bugs

--- app/contexts/ProductsContext.tsx
+++ app/contexts/ProductsContext.tsx
@@ -1,42 +1,48 @@
-import { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState, ReactNode } from 'react';
 import supabase from '../supabase';
 
-const ProductsContext = createContext({
-    products:[],
+interface Product {
+  name: string;
+  price: string;
+  description: string;
+}
+
+interface ProductsContextProps {
+  products: Product[];
+}
+
+const ProductsContext = createContext<ProductsContextProps>({
+  products: [],
 });
 
-export const ProductContextProvider = ({ children }) => { 
+export const ProductContextProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [products, setProducts] = useState<Product[]>([]);
 
-    const [products, setProducts] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { data, error } = await supabase.from('products').select('*');
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const { data, error } = await supabase
-                    .from('products')
-                    .select('*');
-                
-                if (error) {
-                    throw error;
-                }
-
-                setProducts(data);
-            } catch (error: any) {
-                console.error('Error fetching data:', error.message);
-            }
+        if (error) {
+          throw error;
         }
 
-        fetchData();
-    }, []);
+        setProducts(data);
+      } catch (error: any) {
+        console.error('Error fetching data:', error.message);
+      }
+    }
 
-    const contextValue = {
-        products: products,
-    };
+    fetchData();
+  }, []);
 
-    
-    return (
-        <ProductsContext.Provider value={contextValue}>
-            {children}
-        </ProductsContext.Provider>
+  const contextValue: ProductsContextProps = {
+    products: products,
+  };
+
+  return (
+    <ProductsContext.Provider value={contextValue}>
+      {children}
+    </ProductsContext.Provider>
   );
-}
+};
