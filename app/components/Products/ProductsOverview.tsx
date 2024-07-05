0f666ec32534a5949a5dc4c6f3aaa77a7032fbe6
unknown <dejanmaric259@gmail.com>
2024-07-05 15:10:40 +0200

    redesignign products overview page

--- app/components/Products/ProductsOverview.tsx
+++ app/components/Products/ProductsOverview.tsx
@@ -25,26 +25,38 @@ const ProductsOverview = () => {
       .eq("id", id);
   }
 
-  async function editProduct(id: number, newName: string, newPrice: string, newDescription: string, newDiscount: string) {
+  async function editProduct(
+    id: number,
+    newName: string,
+    newPrice: string,
+    newDescription: string,
+    newDiscount: string
+  ) {
     const { error } = await supabase
       .from("products")
-      .upsert({ id: id, name: newName, price: newPrice, description: newDescription, discount: newDiscount })
+      .upsert({
+        id: id,
+        name: newName,
+        price: newPrice,
+        description: newDescription,
+        discount: newDiscount,
+      })
       .eq("id", id);
   }
 
-
-
   return (
     <div className="container mx-auto my-8 px-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="flex flex-wrap gap-4  ">
         {products.map((product) => (
           <div
             key={product.name}
-            className="bg-white p-4 shadow-md rounded-md flex flex-col gap-4 text-slate-950"
+            className="bg-white p-4 shadow-md rounded-md flex flex-col gap-4 text-slate-950 w-72 relative"
           >
-            <img
+            <Image
+              width={256}
+              height={256}
               src={product.image}
-              className="w-full rounded-md"
+              className="rounded-md h-64"
               alt="Product Image"
             />
             {editProductId === product.id && (
@@ -58,38 +70,42 @@ const ProductsOverview = () => {
                 editProduct={editProduct}
               />
             )}
-            <div>
-              <div className="flex items-center gap-4">
-                <p className="text-lg font-semibold">Name: {product.name}</p>
+            <div className="flex items-center gap-4 w-64">
+              <p className="text-lg font-semibold">Name: {product.name}</p>
+              <div
+                className="absolute top-4 right-4 bg-blue-500 p-2 rounded-lg cursor-pointer hover:shadow-lg"
+                onClick={() => setEditProductId(product.id)}
+              >
                 <Icon
-                  onClick={() => setEditProductId(product.id)}
                   icon="material-symbols:edit-outline"
                   width="24"
                   height="24"
-                  style={{ color: "000" }}
+                  style={{ color: "fff" }}
                 />
               </div>
-              <p className="text-gray-700">Price: ${product.price}</p>
-              <p className="text-gray-700">Description: {product.description}</p>
-              <button
-                className="border-2 border-red-700 text-red-700 rounded-md py-1 px-2 mt-2"
-                onClick={() => removeProductFromShop(product.id)}
-              >
-                Remove from the shop
-              </button>
-              <button
-                className="border-2 border-red-700 text-red-700 flex items-center p-2 rounded-md mt-2"
-                onClick={() => deleteProduct(product.id)}
-              >
-                <Icon
-                  icon="material-symbols:delete-outline-rounded"
-                  width="24"
-                  height="24"
-                  style={{ color: "rgb(185 28 28)" }}
-                />
-                Delete product
-              </button>
             </div>
+            <p className="text-gray-700">Price: ${product.price}</p>
+            <p className="text-gray-700 text-md w-64">
+              Description: {product.description}
+            </p>
+            <button
+              className="border-2 border-red-700 text-red-700 rounded-md py-1 px-2 mt-2"
+              onClick={() => removeProductFromShop(product.id)}
+            >
+              Remove from the shop
+            </button>
+            <button
+              className="border-2 border-red-700 text-red-700 flex items-center p-2 rounded-md mt-2"
+              onClick={() => deleteProduct(product.id)}
+            >
+              <Icon
+                icon="material-symbols:delete-outline-rounded"
+                width="24"
+                height="24"
+                style={{ color: "rgb(185 28 28)" }}
+              />
+              Delete product
+            </button>
           </div>
         ))}
       </div>
--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['assets.swappie.com', 'itshop.ba', 'genelec.ba' , 'sync.ba' , 'cpuinfotech.ba' , 'm.media-amazon.com'],
-    }
-}
+  images: {
+    domains: [
+      "assets.swappie.com",
+      "itshop.ba",
+      "genelec.ba",
+      "sync.ba",
+      "cpuinfotech.ba",
+      "m.media-amazon.com",
+      "images.unsplash.com",
+    ],
+  },
+};
 
 module.exports = nextConfig;
